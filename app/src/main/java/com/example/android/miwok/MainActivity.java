0e9a1b4c33cff57428442ec993d52c98f0bf64bf
Alienx17 <radu.costan17@yahoo.com>
2019-10-08 20:42:43 +0300

    Initial commit

---
+++ app/src/main/java/com/example/android/miwok/MainActivity.java
@@ -0,0 +1,50 @@
+package com.example.android.miwok;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // Set the content of the activity to use the activity_main.xml layout file
+        setContentView(R.layout.activity_main);
+
+        //Find the View that shows the numbers category
+        TextView numbers = findViewById(R.id.numbers);
+
+        //Set a clicklistener to that view
+        numbers.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View view){
+                //Create a new intent to open the NumbersActivity
+                Intent numbersIntent = new Intent(MainActivity.this,NumbersActivity.class);
+
+                //Start the new activity
+                startActivity(numbersIntent);
+
+            }
+        });
+    }
+
+    public void openFamilyList(View view){
+        Intent i = new Intent(this, FamilyActivity.class);
+        startActivity(i);
+    }
+    public void openColorsList(View view){
+        Intent i = new Intent(this, ColorsActivity.class);
+        startActivity(i);
+    }
+    public void openPhrasesList(View view){
+        Intent i = new Intent(this, PhrasesActivity.class);
+        startActivity(i);
+    }
+
+}
---
+++ app/src/main/java/com/example/android/miwok/Word.java
@@ -0,0 +1,49 @@
+package com.example.android.miwok;
+
+class Word {
+
+    private String mDefaultTranslation;
+
+    private String mMiwokTranslation;
+
+    private int mAudioResourceId;
+
+    private int mImageResourceId = NO_IMAGE_PROVIDED;
+
+    private static final int NO_IMAGE_PROVIDED = 0;
+
+    //Constructor pentru cuvinte
+    Word(String defaultTranslation, String miwokTranslation, int audioResourceId){
+        mDefaultTranslation = defaultTranslation;
+        mMiwokTranslation = miwokTranslation;
+         mAudioResourceId = audioResourceId;
+    }
+
+    //Constructor pentru imagini
+    Word(String defaultTranslation, String miwokTranslation, int imageResourceId, int audioResourceId){
+        mDefaultTranslation = defaultTranslation;
+        mMiwokTranslation = miwokTranslation;
+        mImageResourceId = imageResourceId;
+        mAudioResourceId = audioResourceId;
+    }
+
+    String getDefaultTranslation(){
+        return mDefaultTranslation;
+    }
+
+    String getMiwokTranslation(){
+        return mMiwokTranslation;
+    }
+
+    int getmImageResourceId(){
+        return mImageResourceId;
+    }
+
+    boolean hasImage(){
+        return mImageResourceId != NO_IMAGE_PROVIDED;
+    }
+
+    int getAudioResourceId(){
+        return mAudioResourceId;
+    }
+}
